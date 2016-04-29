8ad1749cc0f687f4d00c7fc6e800824b8aae4acc
Zachary Bears <bears.zachary@gmail.com>
2016-04-29 19:20:54 -0400

    Implemented nonhomogenous shell selection

--- GroupAssignment2/ShapeStatistics.py
+++ GroupAssignment2/ShapeStatistics.py
@@ -92,16 +92,20 @@ def getShapeHistogram(Ps, Ns, NShells, RMax):
 #sectors within each shell, sorted in decreasing order of number of points
 #Inputs: Ps (3 x N point cloud), Ns (3 x N array of normals) (not needed here
 #but passed along for consistency), NShells (number of shells), 
-#RMax (maximum radius), SPoints: A 3 x S array of points sampled evenly on 
+#RMax (maximum radius), SPoints: A 3 x S array of points sampled evenly on
+#RMin (minimum radius), 
 #the unit sphere (get these with the function "getSphereSamples")
-# NOTE: THIS WAS CHANGED FROM THE ORIGINAL
-def getShapeShellHistogram(Ps, Ns, NShells, RMax, SPoints):
+def getShapeShellHistogram(Ps, Ns, NShells,RMin, RMax, SPoints):
+    # Remove any points below RMin from the origin
+    pointDistances = np.linalg.norm(Ps, axis=0)
+    filteredElems = Ps[:,pointDistances>=RMin]
+
     NSectors = SPoints.shape[1] #A number of sectors equal to the number of
     #points sampled on the sphere
     # First, dot product all points in Ps against all points in SPoints.
     # We're doing a matrix multiplication of Ps^T with SPoints. This will give an NxM matrix
     # Where the rows are the dot product magnitudes of each point of Ps with all the points in SPoints
-    dots = np.dot(Ps.T, SPoints)
+    dots = np.dot(filteredElems.T, SPoints)
     # Then find the index of the point in SPoints that yields the largest dot product for each Ps point
     maximums = np.argmax(dots,axis=1).T
     #Create a 2D histogram that is NShells x NSectors
@@ -109,11 +113,39 @@ def getShapeShellHistogram(Ps, Ns, NShells, RMax, SPoints):
     hist = np.zeros((NShells, 0)) 
     # Go through each sector and create a histogram
     for i in range(NSectors):
-        sectorElems = Ps[:,maximums==i] # Select every element in the given sector
-        sectorHistogram=np.array(np.histogram(np.linalg.norm(sectorElems,axis=0),NShells,(0,RMax))[0])[np.newaxis].T # Create a histogram for the sector in Column form
+        sectorElems = filteredElems[:,maximums==i] # Select every element in the given sector
+        sectorHistogram=np.array(np.histogram(np.linalg.norm(sectorElems,axis=0),NShells,(RMin,RMax))[0])[np.newaxis].T # Create a histogram for the sector in Column form
         hist=np.hstack((hist,sectorHistogram)) # Add the column to the histogram
       
     return hist
+
+#Purpose: To create a shape shell histogram using differential sampling of concentric spherical shells.
+# meant to differentiate the sampling volume difference for further away sectors
+# Note: Rmax is automatically calculated by this algorithm
+#Inputs: Ps (3 x N point cloud), Ns (3 x N array of normals) (not needed here
+#but passed along for consistency), NShells (number of shells), 
+#RMax (maximum radius), SPointRange: A range of spoint sampling rates to be spread linearly
+#amongst the shells in RMax. i.e. if 4 numbers are given in the range, the first will be used for 0-RMax/4
+#Returns an array of tuples of (innerShellRadius, outerShellRadius, SValue, ShapeShellHistogram, ShellWidth)
+
+def getDifferentialShapeShellHistogram(Ps, Ns, NShells, SPointRange):
+    pointDistances = np.linalg.norm(Ps, axis=0)
+    Rmax = np.amax(pointDistances,) 
+    # Figure out how many different sampling rates we're dealing with
+    numSampleIncrements = len(SPointRange)
+    distanceIncrement = 1.0*Rmax/numSampleIncrements
+    shellIncrement = 1.0*NShells/numSampleIncrements
+    if (math.ceil(shellIncrement)!=math.floor(shellIncrement)):
+        print "Warning: NShells should be integer divisible by the number of sample increments"
+    shellIncrement=math.floor(shellIncrement)
+    histogram = []
+    # Run the shape shell histogram on each shell
+    for i in range(numSampleIncrements):
+        RMin=i*distanceIncrement
+        RMax=(i+1)*distanceIncrement
+        sampleHistogram=getShapeShellHistogram(Ps,Ns,shellIncrement,RMin, RMax, getSphereSamples(SPointRange[i]))
+        histogram.append((RMin,RMax,SPointRange[i],sampleHistogram, distanceIncrement/shellIncrement))
+    return histogram
 
 #Purpose: To create shape histogram with concentric spherical shells and to 
 #compute the PCA eigenvalues in each shell
@@ -321,53 +353,61 @@ def getMinimumAngleAmongPoints(SPoints):
     
 #Returns a point cloud from a 2D histogram
 #The point cloud is generated with random sampling
-def getSamplePointsFrom2DHistogram(numSamples, hist, shellWidth):
-    res = 3 # this should correspond to the resolution of the original sampling in making the histogram
-    SPoints = getSphereSamples(res)
-    
-    # this is the angle between the two nearest sampled points
-    # which is the same as the angle of the sampled cone around the vectors
-    angle = getMinimumAngleAmongPoints(SPoints)
-    
-    # get flattened matrix of probability from the densities
-    flatProbabilities = (1.0*hist/np.sum(hist)).flatten()
-    # get random samples of indexes of sectors
-    randomSectorIndexes = np.random.choice(len(flatProbabilities), numSamples, p=flatProbabilities)
-    
-    histWidth = hist.shape[1]
-    
-        # initialize Ps
+# numSamples is the overall number of sectors to use in the sampling
+# hist is the histogram in the format ((Rmin,Rmax,SValue, Histogram)) for a special shell sampled with differential sampling
+def getSamplePointsFrom2DHistogram(numSamples, histogram):
+    numSections = len(histogram)
+    pointsPerSection = numSamples/numSections
+    # initialize Ps
     Ps = np.zeros((3,numSamples))
-    
-    for i in range(0, len(randomSectorIndexes)):
-        index = randomSectorIndexes[i]
-        # calculate the INDEXES of the shell and sector
-        shellIndex = index / histWidth # index of shell in hist
-        sectorIndex = index % histWidth  # index of sector in hist
-
-        #TODO find radius range, phi range, and theta range, and randomly sample in them to determine new point location
-        # shell is zero indexed
-        radiusMin = shellIndex*shellWidth
-        radiusMax = (shellIndex+1)*shellWidth
-
-        # phi, theta location somewhere around the sphere sampled vector
-        sector = SPoints[:, sectorIndex] # I think? might be backwards
-        [_theta, _phi] = convertRectangularToPolar(sector[0], sector[1], sector[2])
-        phiMin = _phi - angle/2.0
-        phiMax = _phi + angle/2.0
-        thetaMin = _theta - angle/2.0
-        thetaMax = _theta + angle/2.0
+    sampleNum=0
+    for j in range(numSections):
+        shellHist = histogram[j]
+        RMin = shellHist[0]
+        RMax = shellHist[1]
+        SVal = shellHist[2]
+        hist = shellHist[3]
+        shellWidth = shellHist[4]
+        SPoints = getSphereSamples(SVal)
+        # this is the angle between the two nearest sampled points
+        # which is the same as the angle of the sampled cone around the vectors
+        angle = getMinimumAngleAmongPoints(SPoints)
+        # get flattened matrix of probability from the densities
+        flatProbabilities = (1.0*hist/np.sum(hist)).flatten()
+        # get random samples of indexes of sectors
+        randomSectorIndexes = np.random.choice(len(flatProbabilities), pointsPerSection, p=flatProbabilities)
+        histWidth = hist.shape[1]
         
-        # randomly sample these
-        randomRadius = (radiusMax - radiusMin) * np.random.random_sample() + radiusMin
-        randomPhi = (phiMax - phiMin) * np.random.random_sample() + phiMin
-        randomTheta = (thetaMax - thetaMin) * np.random.random_sample() + thetaMin
-        
-        # convert returns [x, y, z]
-        ret = convertPolarToRectangular(randomRadius, randomPhi, randomTheta)
-        Ps[0,i] = ret[0]
-        Ps[1,i] = ret[1]
-        Ps[2,i] = ret[2]
+        for i in range(0, len(randomSectorIndexes)):
+            index = randomSectorIndexes[i]
+            # calculate the INDEXES of the shell and sector
+            shellIndex = index / histWidth # index of shell in hist
+            sectorIndex = index % histWidth  # index of sector in hist
+            
+            # Radius range, phi range, and theta range, and randomly sample in them to determine new point location
+            # shell is zero indexed
+            radiusMin = RMin+shellIndex*shellWidth
+            radiusMax = RMin+(shellIndex+1)*shellWidth
+
+            # phi, theta location somewhere around the sphere sampled vector
+            sector = SPoints[:, sectorIndex]
+            [_theta, _phi] = convertRectangularToPolar(sector[0], sector[1], sector[2])
+            phiMin = _phi - angle/2.0
+            phiMax = _phi + angle/2.0
+            thetaMin = _theta - angle/2.0
+            thetaMax = _theta + angle/2.0
+            
+            # randomly sample these
+            randomRadius = (radiusMax - radiusMin) * np.random.random_sample() + radiusMin
+            randomPhi = (phiMax - phiMin) * np.random.random_sample() + phiMin
+            randomTheta = (thetaMax - thetaMin) * np.random.random_sample() + thetaMin
+            
+            # convert returns [x, y, z]
+            ret = convertPolarToRectangular(randomRadius, randomPhi, randomTheta)
+            Ps[0,sampleNum] = ret[0]
+            Ps[1,sampleNum] = ret[1]
+            Ps[2,sampleNum] = ret[2]
+            sampleNum+=1
         
     return Ps
         
@@ -853,6 +893,14 @@ def runRandomComparisonExperiments():
     plt.legend()
     plt.show()
     
+# Calculate the size of a histogram. This is used for extimating space savings
+def calculateHistogramSize(histogram):
+    size=0
+    for i in range(len(histogram)):
+        size+=histogram[i][3].shape[0]*histogram[i][3].shape[1]
+    return size
+
+
 #########################################################
 ##                     MAIN TESTS                      ##
 #########################################################
@@ -863,15 +911,24 @@ if __name__ == '__main__':
     np.random.seed(100) #For repeatable results randomly sampling
     #Load in the mesh we care about
     m = PolyMesh()
-    filename = "models_off/biplane0.off"
+    filename = "models_off/race_car0.off"
     m.loadOffFileExternal(filename)
     (Ps, Ns) = samplePointCloud(m, NRandSamples)
     exportPointCloud(Ps,Ns,"originalPointCloud.pts")
-    num_shells = 100
-    RMax = 10
-    # I set num shells to 100, the distance to 10, and the number of sphere samples to 258.
-    histogram = getShapeShellHistogram(Ps, Ns, num_shells, RMax,getSphereSamples(3))
-    # Note this method assumes res is equal to 3. Sampling 1000 samples. 
-    newPointCloud = getSamplePointsFrom2DHistogram(NRandSamples, histogram, 1.0*RMax/num_shells)
+    num_shells = 51
+
+    bigHistogram = getDifferentialShapeShellHistogram(Ps, Ns, num_shells,(5,))
+    scaledHistogram = getDifferentialShapeShellHistogram(Ps, Ns, num_shells,(3,4,5))
+    # Calculate savings versus a traditional histogram
+    newPointCloud = getSamplePointsFrom2DHistogram(NRandSamples, bigHistogram)
+    savings = calculateHistogramSize(scaledHistogram)*1.0/calculateHistogramSize(bigHistogram)
+    print savings
+    # Calculate savings versus the original mesh
+    originalSavings = calculateHistogramSize(scaledHistogram)*1.0/(3*NRandSamples)
+    print originalSavings
+
     # Now we'll export this point cloud. Normals will be completely wrong because we never calculated them
     exportPointCloud(newPointCloud,Ns,"ReconstructedCloud.pts")
+    newPointCloud = getSamplePointsFrom2DHistogram(NRandSamples, scaledHistogram)
+    # Now we'll export this point cloud. Normals will be completely wrong because we never calculated them
+    exportPointCloud(newPointCloud,Ns,"ReconstructedCloudScaling.pts")
